904c9b80105caf646aeadbd8502f2ee87304228f
Marko Petrović <90247041+MarkoP02@users.noreply.github.com>
2022-07-22 13:00:13 +0200

    Add files via upload

---
+++ js/Comment.js
@@ -0,0 +1,46 @@
+class Comment {
+    username = '';
+    post_id = '';
+    user_id = '';
+    content = '';
+    api_url = 'https://62d6b1fa51e6e8f06f10a998.mockapi.io';
+
+    create() {
+        let data = {
+            username: this.username,
+            post_id: this.post_id,
+            user_id: this.user_id,
+            content: this.content
+        };
+
+        data = JSON.stringify(data);
+
+        fetch(this.api_url + '/comments', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: data
+        })
+        .then(Response => Response.json())
+        .then(data => {alert('Postavljen komentar')});
+    }
+
+    async get(post_id) {
+        let api_url = this.api_url + '/comments';
+
+        const response = await fetch(api_url);
+        const data = await response.json();
+        let post_comments = [];
+
+        let i = 0;
+        data.forEach(item => {
+            if(item.post_id === post_id) {
+                post_comments[i] = item;
+                i++;
+            }
+        });
+
+        return post_comments;
+    }
+}
